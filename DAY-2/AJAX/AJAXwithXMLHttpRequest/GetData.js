5fc1c334eee505b1b35a7c825d96074110536cb3
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-06-13 11:29:31 +0530

    ✨ AJAX basics - XMLHttpRequest

--- DAY-2/AJAX/AJAXwithXMLHttpRequest/GetData.js
+++ DAY-2/AJAX/AJAXwithXMLHttpRequest/GetData.js
@@ -1,4 +1,15 @@
 function GetData() {
-  // AJAX Logic
-  console.log("AJAX logic here");
+  // 1. Instanciate XMLHttpRequest
+  // 2. Open a connection
+  // 3. To register on an event - onreadystatechange
+  // 4. To make the async call (send)
+
+  let xmlHttpReq = new XMLHttpRequest();
+  xmlHttpReq.open("GET", "https://jsonplaceholder.typicode.com/posts");
+  xmlHttpReq.onreadystatechange = function () {
+    if (xmlHttpReq.readyState == 4 && xmlHttpReq.status == 200) {
+      console.log(xmlHttpReq.responseText);
+    }
+  };
+  xmlHttpReq.send(); // places the async call
 }
