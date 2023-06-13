a15353e2e8770d4d7047ed83dcdbae2184392679
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-06-13 11:54:39 +0530

    ✨ Using Callbacks

--- DAY-2/AJAX/AJAXwithXMLHttpRequest/GetData.js
+++ DAY-2/AJAX/AJAXwithXMLHttpRequest/GetData.js
@@ -1,4 +1,4 @@
-function GetData() {
+function GetData(callback) {
   // 1. Instanciate XMLHttpRequest
   // 2. Open a connection
   // 3. To register on an event - onreadystatechange
@@ -8,7 +8,8 @@ function GetData() {
   xmlHttpReq.open("GET", "https://jsonplaceholder.typicode.com/posts");
   xmlHttpReq.onreadystatechange = function () {
     if (xmlHttpReq.readyState == 4 && xmlHttpReq.status == 200) {
-      console.log(xmlHttpReq.responseText);
+      //console.log(xmlHttpReq.responseText);
+      callback(xmlHttpReq.responseText);
     }
   };
   xmlHttpReq.send(); // places the async call
