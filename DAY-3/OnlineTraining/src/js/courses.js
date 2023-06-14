fb32887cb51302a9b17465376b892c87af6781bf
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-06-14 14:10:50 +0530

    ✨ Boostrap Card - Courses

--- DAY-3/OnlineTraining/src/js/courses.js
+++ DAY-3/OnlineTraining/src/js/courses.js
@@ -22,7 +22,7 @@ function CreateNewCourseItem(course) {
 
   let newCourseCard = document.createElement("div");
   newCourseCard.className = "card";
-  newCourseCard.style.width = "18rem";
+  newCourseCard.style.width = "15rem";
 
   let newCourseCardImage = document.createElement("img");
   newCourseCardImage.src = course.imageUrl;
@@ -30,6 +30,31 @@ function CreateNewCourseItem(course) {
   newCourseCardImage.className = "card-img-top";
   newCourseCard.appendChild(newCourseCardImage);
 
+  let newCourseCardBody = document.createElement("div");
+  newCourseCardBody.className = "card-body";
+
+  let courseTitle = document.createElement("h5");
+  courseTitle.className = "card-title";
+  courseTitle.innerText = course.title;
+
+  let coursePrice = document.createElement("p");
+  coursePrice.innerText = "₹." + course.price;
+  coursePrice.className = "card-text";
+
+  let courseRating = document.createElement("p");
+  courseRating.innerText = course.rating;
+  courseRating.className = "card-text";
+
+  let courseLikesBtn = document.createElement("button");
+  courseLikesBtn.innerText = course.likes;
+  courseLikesBtn.className = "btn btn-primary";
+
+  newCourseCardBody.appendChild(courseTitle);
+  newCourseCardBody.appendChild(coursePrice);
+  newCourseCardBody.appendChild(courseRating);
+  newCourseCardBody.appendChild(courseLikesBtn);
+
+  newCourseCard.appendChild(newCourseCardBody);
   listofcourses.appendChild(newCourseCard);
 }
 
