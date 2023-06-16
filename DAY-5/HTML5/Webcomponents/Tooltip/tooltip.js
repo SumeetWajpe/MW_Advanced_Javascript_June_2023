3094f81c3146cc41a47629f30316c4fb3d850ede
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-06-16 12:52:49 +0530

    ✨ Tooltip - Custom Element -Handling Events

---
+++ DAY-5/HTML5/Webcomponents/Tooltip/tooltip.js
@@ -0,0 +1,39 @@
+class Tooltip extends HTMLElement {
+  constructor() {
+    super();
+    this.tooltip = null;
+  }
+
+  connectedCallback() {
+    let tooltipIcon = document.createElement("span");
+    tooltipIcon.innerHTML = "(?)";
+    tooltipIcon.addEventListener("mouseenter", () => this.showTooltip());
+    tooltipIcon.addEventListener("mouseleave", () => this.hideTooltip());
+
+    this.style.position = "relative";
+    this.appendChild(tooltipIcon);
+  }
+
+  showTooltip() {
+    this.tooltip = document.createElement("div");
+    this.tooltip.innerText = "Dummy Tooltip !";
+    this.tooltip.style.backgroundColor = "black";
+    this.tooltip.style.color = "white";
+    this.tooltip.style.border = "2px solid grey";
+    this.tooltip.style.borderRadius = "3px";
+    this.tooltip.style.padding = "3px";
+    this.tooltip.style.position = "absolute";
+    this.tooltip.style.left = "10px";
+    this.tooltip.style.top = "-10px";
+    this.tooltip.style.fontSize = "10px";
+    this.tooltip.style.width = "max-content";
+
+    this.appendChild(this.tooltip);
+  }
+
+  hideTooltip() {
+    this.removeChild(this.tooltip);
+  }
+}
+
+customElements.define("uc-tooltip", Tooltip);
