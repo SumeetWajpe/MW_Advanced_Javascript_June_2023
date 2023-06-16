febb584f43464ca34c8385afa01540695d3dbe03
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-06-16 11:09:22 +0530

    ✨ Web Workers

---
+++ DAY-5/HTML5/Webworkers/worker.js
@@ -0,0 +1,11 @@
+onmessage = function (msgFromMainThread) {
+  console.log(msgFromMainThread.data);
+  let arr = [];
+  for (let index = 0; index < 10000; index++) {
+    arr[index] = [];
+    for (let j = 0; j < 15000; j++) {
+      arr[index][j] = Math.random();
+    }
+  }
+  postMessage(arr[3000][4000]);
+};
